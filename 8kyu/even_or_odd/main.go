c294cbb4823181cfae24b01a040728faf0a168c1
Arsen Hovhannisyan <arsen.hovhannisyan.1991@gmail.com>
2025-11-14 15:47:54 +0800

    Refactor IsEven function for clarity and maintainability

--- 8kyu/even_or_odd/main.go
+++ 8kyu/even_or_odd/main.go
@@ -1,11 +1,11 @@
 package even_or_odd
 
-func IsEvenOrOdd(number int) bool {
-	return number%2 == 0
+func IsEven(number int) bool {
+	return (number % 2) == 0
 }
 
 func EvenOrOdd(number int) string {
-	if IsEvenOrOdd(number) {
+	if IsEven(number) {
 		return "Even"
 	}
 
